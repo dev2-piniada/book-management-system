a6a381993a8d2f00bb4a1e53d4272907f33aecfc
Saneera Yapa <saneera.yapa@nuevotek.com>
2025-03-06 08:40:42 +1030

    all

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BookModel } from '../models/book.model';
 
 @Component({
   selector: 'app-book',
@@ -10,9 +11,25 @@ export class BookComponent {
   title: string = "";
   author: string = "";
 
+  books :BookModel[] = [] 
+
   addBook() {
     console.log("Title: " + this.title);
     console.log("Author: " + this.author);
+    let book: BookModel = {
+      id: this.books.length + 1, 
+      title: this.title, 
+      author: this.author
+    };
+    this.books.push(book);
+    this.title = "";
+    this.author = "";
+  }
+
+  removeBook(id: number) {
+    this.books = this.books.filter(book => book.id !==
+    id);  
+
   }
 
 }
